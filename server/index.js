b8d385f1ed95d73d7f04b127ba84448401fb18fc
Haroon Syed <63178444+Hxsyed@users.noreply.github.com>
2022-01-18 20:51:26 -0500

    slight change

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 
 const app = express();
 app.use(express.json()); 
-app.use(cors());
+// app.use(cors());
 
 const corsOptions ={
     origin: process.env.HEROKU_LINK, 
